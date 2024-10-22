e897d878db9a09804226cbac92e3363351e4dec2
Fernando Olguin <f.olguin.ch@gmail.com>
2024-10-22 13:28:23 +0900

    Implement auto-masking

--- goco_helpers/clean_tasks.py
+++ goco_helpers/clean_tasks.py
@@ -1,6 +1,6 @@
 """Tools for running the `tclean` CASA task."""
 #from typing import Any, Callable, List, Optional, Sequence, TypeVar, Mapping
-from typing import Callable, Sequence
+from typing import Callable, Sequence, Dict
 from datetime import datetime
 from pathlib import Path
 import json
@@ -12,20 +12,36 @@ from astropy.stats import mad_std
 from casatasks import tclean, exportfits #, imsubimage
 import astropy.units as u
 #from casatools import image
-#import numpy as np
+import numpy as np
 
 from .common_types import SectionProxy
 #from .data_handler import DataHandler
 from .utils import get_func_params#, iter_data
 
+def image_sn(fitsimage: Path,
+             default_unit: u.Unit = u.mJy/u.beam
+             ) -> Tuple[u.Quantity, u.Quantity]:
+    """Calculate the peak and noise of an image."""
+    image = fits.open(fitsimage)[0]
+    unit = u.Unit(image.header['BUNIT'])
+    peak = np.nanmax(image.data) * unit
+    peak = peak.to(default_unit)
+    rms = mad_std(image.data, ignore_nan=True) * unit
+    rms = rms.to(default_unit)
+
+    return peak, rms
+
 def get_tclean_params(
     config: SectionProxy,
     required_keys: Sequence[str] = ('cell', 'imsize'),
     ignore_keys: Sequence[str] = ('vis', 'imagename', 'spw'),
-    float_keys: Sequence[str]  = ('robust', 'pblimit', 'pbmask'),
+    float_keys: Sequence[str]  = ('robust', 'pblimit', 'pbmask',
+                                  'noisethreshold', 'sidelobethreshold',
+                                  'lownoisethreshold', 'minbeamfrac',
+                                  'negativethreshold'),
     int_keys: Sequence[str] = ('niter', 'chanchunks'),
     bool_keys: Sequence[str] = ('interactive', 'parallel', 'pbcor',
-                                'perchanweightdensity'),
+                                'perchanweightdensity', 'fastnoise'),
     int_list_keys: Sequence[str] = ('imsize', 'scales'),
 ) -> dict:
     """Filter input parameters and convert values to the correct type.
@@ -48,6 +64,65 @@ def get_tclean_params(
         tclean_pars['imsize'] = tclean_pars['imsize'] * 2
 
     return tclean_pars
+
+def automasking_params(b75: u.Quantity,
+                       is_cube: bool,
+                       is_aca: bool = False,
+                       is_combined: bool = False) -> Dict:
+    """Get recommended auto-masking parameters.
+
+    Parameters are taken from the table in the [CASA guides](
+    https://casaguides.nrao.edu/index.php/Automasking_Guide_CASA_6.6.1)
+
+    Args:
+      b75: Baseline at 75th percentile.
+      is_cube: Is imaging for cubes?
+      is_aca: Optional. Is the data from ACA?
+      is_combined: Optional. Is the data from 12m and 7m arrays?
+
+    Returns:
+      A dictionary with the parameter values.
+    """
+    if is_aca:
+        parameters = {'noisethreshold': 5.0,
+                      'sidelobethreshold': 1.25,
+                      'lownoisethreshold': 2.0,
+                      'minbeamfrac': 0.1,
+                      'negativethreshold': 0.0,
+                      'fastnoise': False}
+    elif is_combined:
+        parameters = {'noisethreshold': 4.25,
+                      'sidelobethreshold': 2.0,
+                      'lownoisethreshold': 1.5,
+                      'minbeamfrac': 0.3,
+                      'negativethreshold': 0.0,
+                      'fastnoise': False}
+    else:
+        if b75 < 300 * u.m:
+            negativethreshold = 15. if is_cube else 0.
+            parameters = {'noisethreshold': 4.25,
+                          'sidelobethreshold': 2.0,
+                          'lownoisethreshold': 1.5,
+                          'minbeamfrac': 0.3,
+                          'negativethreshold': negativethreshold,
+                          'fastnoise': False}
+        elif b75 < 400 * u.m:
+            negativethreshold = 7. if is_cube else 0.
+            parameters = {'noisethreshold': 5.0,
+                          'sidelobethreshold': 2.0,
+                          'lownoisethreshold': 1.5,
+                          'minbeamfrac': 0.3,
+                          'negativethreshold': negativethreshold,
+                          'fastnoise': False}
+        else:
+            negativethreshold = 7. if is_cube else 0.
+            parameters = {'noisethreshold': 5.0,
+                          'sidelobethreshold': 2.5,
+                          'lownoisethreshold': 1.5,
+                          'minbeamfrac': 0.3,
+                          'negativethreshold': negativethreshold,
+                          'fastnoise': True}
+    return parameters
 
 def tclean_parallel(vis: Sequence[Path],
                     imagename: Path,
@@ -94,18 +169,113 @@ def tclean_parallel(vis: Sequence[Path],
 
 def pb_clean(vis: Sequence[Path],
              imagename: Path,
-             nproc: int,
+             nproc: int = 1,
              nsigma: float = 3.,
              thresh_niter: float = 2,
              log: Callable = print,
-             **tclean_args: dict):
-    """Perform cleaning with a PB mask."""
-    # CLEAN args
+             pbmask: float = 0.2,
+             **tclean_args: Dict) -> Dict:
+    """Perform cleaning with a PB mask.
+
+    Args:
+      vis: List of visibilities.
+      imagename: Base name for image name.
+      nproc: Optional. Number of parallel processes.
+      nsigma: Optional. Noise (rms) level for the threshold.
+      thresh_niter: Optional. Number of iteration to perform.
+      log: Optional. Logging function.
+      pbmask: Optional. Mask PB limit.
+      tclean_args: Optional. Additional arguments for `tclean`.
+
+    Returns:
+      A dictionary with the FITS file name and last threshold.
+    """
     pb_clean_args = {'usemask': 'pb',
-                     'pbmask': 0.2}
+                     'pbmask': pbmask}
     tclean_args.update(pb_clean_args)
+    info = iter_clean(vis, imagename, nproc=nproc, nsigma=nsigma,
+                      thresh_niter=thresh_niter, log=log, **tclean_args)
+
+    return info
+
+def auto_masking(vis: Sequence[Path],
+                 imagename: Path,
+                 nproc: int = 1,
+                 b75: Optional[u.Quantity] = None,
+                 is_aca: bool = False,
+                 is_combined: bool = False,
+                 nsigma: float = 3.,
+                 thresh_niter: float = 2,
+                 log: Callable = print,
+                 **tclean_args: dict) -> Dict:
+    """Clean images iteratively using CASA auto-masking.
+
+    The `b75` parameter (75th percentile baseline) is required to determine the
+    automasking parameters from the recommended values in the [CASA guides](
+    https://casaguides.nrao.edu/index.php/Automasking_Guide_CASA_6.6.1). If not
+    given the `tclean` defaults are used or the values given in `tclean_args`.
+
+    Args:
+      vis: List of visibilities.
+      imagename: Base name for image name.
+      nproc: Optional. Number of parallel processes.
+      b75: Optional. Baseline at 75th percentile.
+      is_aca: Optional. Is the data from ACA?
+      is_combined: Optional. Is the data from the 12m and 7m arrays?
+      nsigma: Optional. Noise (rms) level for the threshold.
+      thresh_niter: Optional. Number of iteration to perform.
+      log: Optional. Logging function.
+      tclean_args: Optional. Additional arguments for `tclean`.
+
+    Returns:
+      A dictionary with the FITS file name and last threshold.
+    """
+    # Get auto-masking parameters
+    clean_args = {'usemask': 'auto-multithresh'}
+    if b75 is not None:
+        is_cube = 'cube' in tclean_args.get('specmode', 'mfs')
+        clean_args.update(automasking_params(b75, is_cube, is_aca=is_aca,
+                                             is_combined=is_combined))
+
+    # Clean iteratively
+    tclean_args.update(clean_args)
+    info = iter_clean(vis, imagename, nproc=nproc, nsigma=nsigma,
+                      thresh_niter=thresh_niter, log=log, **tclean_args)
+
+    return info
+
+def iter_clean(vis: Sequence[Path],
+               imagename: Path,
+               nproc: int = 1,
+               nsigma: float = 3.,
+               thresh_niter: float = 2,
+               log: Callable = print,
+               **tclean_args: dict) -> Dict:
+    """Clean visibilities iteratively.
+
+    After each iteration the threshold is recalculated based on the current rms
+    level.
+
+    Args:
+      vis: List of visibilities.
+      imagename: Base name for image name.
+      nproc: Optional. Number of parallel processes.
+      nsigma: Optional. Noise (rms) level for the threshold.
+      thresh_niter: Optional. Number of iteration to perform.
+      log: Optional. Logging function.
+      tclean_args: Optional. Additional arguments for `tclean`.
+
+    Returns:
+      A dictionary with the final image file name (`fitsimage`), threshold for
+      each step (`thresholds`).
+    """
+    # CLEAN args
     niter = tclean_args.get('niter', 100000)
-    clean_imagename = imagename.with_suffix(f'{imagename.suffix}.image')
+    if tclean_args.get('deconvolver', 'hogbom') == 'mtmfs':
+        clean_imagename = imagename.with_suffix(f'{imagename.suffix}.image.tt0')
+    else:
+        clean_imagename = imagename.with_suffix(f'{imagename.suffix}.image')
+    thresholds = []
 
     for i in range(thresh_niter):
         # First pass
@@ -116,27 +286,42 @@ def pb_clean(vis: Sequence[Path],
             tclean_args['niter'] = niter
             tclean_args['calcres'] = False
             tclean_args['calcpsf'] = False
-        tclean_parallel([vis], imagename, nproc, tclean_args, log=log)
-        fitsimage = clean_imagename.with_suffix(f'.threshniter{i}.image.fits')
+        tclean_parallel(vis, imagename, nproc, tclean_args, log=log)
+        fitsimage = clean_imagename.with_suffix(f'.iter{i}.image.fits')
         exportfits(imagename=f'{clean_imagename}', fitsimage=f'{fitsimage}',
-                overwrite=True)
+                   overwrite=True)
 
         # Get rms
-        image = fits.open(fitsimage)[0]
-        rms = mad_std(image.data, ignore_nan=True) * u.Unit(image.header['BUNIT'])
-        rms = rms.to(u.mJy/u.beam)
-        threshold = nsigma * rms * u.beam
-        log(f'Threshold iteration {i}: {threshold}')
+        _, rms = image_sn(fitsimage)
+        thresholds.append(nsigma * rms * u.beam)
+        log(f'Threshold from iteration {i}: {threshold[i]}')
+
+        # Check threshold divergence
+        if i > 0 and thresholds[i] > thresholds[i-1]:
+            log('Thresholds diverging, CLEAN finished')
+            final_fitsimage = clean_imagename.with_suffix(f'.final.image.fits')
+            os.system('mv {fitsimage} {final_fitsimage}')
+            return {'fitsimage': final_fitsimage, 'thresholds': thresholds}
 
         # Set threshold
-        tclean_args['threshold'] = f'{threshold.value}{threshold.unit}'
+        tclean_args['threshold'] = f'{threshold[i].value}{threshold[i].unit}'
 
     # Final run
     tclean_args['niter'] = niter
     tclean_parallel([vis], imagename, nproc, tclean_args, log=log)
-    fitsimage = clean_imagename.with_suffix(f'.image.fits')
+    fitsimage = clean_imagename.with_suffix(f'.final.image.fits')
     exportfits(imagename=f'{clean_imagename}', fitsimage=f'{fitsimage}',
                overwrite=True)
+
+    # Pbcor image to fits
+    if tclean_args.get('pbcor', False):
+        pbcorimage = clean_imagename.with_suffix(
+            f'{clean_imagename.suffix}.pbcor')
+        pbcor_fits_image = pbcorimage.with_suffix(f'.final.image.pbcor.fits')
+        exportfits(imagename=f'{pbcorimage}', fitsimage=f'{pbcor_fits_image}',
+                   overwrite=True)
+
+    return {'fitsimage': fitsimage, 'thresholds': thresholds}
 
 #def compute_dirty(data: Sequence['DataHandler'],
 #                  dirty_dir: Path,
